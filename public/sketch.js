d48b633be0326eaa90c2d7a32353fdd514e82d31
Umezawa Wataru <hoshiika141592@gmail.com>
2020-07-28 19:08:14 +0900

    make it better

--- public/sketch.js
+++ public/sketch.js
@@ -196,7 +196,7 @@ function reaction(num) {
   $('ul').append('<li class="chat" id="logNo' + chatcount + '"><div class="my"><div class="coment" id="chatNo' + chatcount + '"><span class=re>Re:</span>' + text + '</div></div></li>');
   let reId = '#chatNo' + chatcount;
   let c = rgb2css(ang, neu, smi);
-  console.log(reID);
+  console.log(reId);
   $(reId).css("background-color", c);
   chatcount++;
 }
